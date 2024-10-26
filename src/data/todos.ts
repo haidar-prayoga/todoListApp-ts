f5b694abecd69eddfe4d16ac7cff9a01743843c9
Haidar <haidarprayoga9b@gmail.com>
2024-10-27 01:58:03 +0700

    first commit

---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+  {
+    id: 1,
+    title: "Like this video 👍",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Subscribe to my channel 🔥",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Join my Discord server 🤖",
+    completed: false,
+  },
+];
